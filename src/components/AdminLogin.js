f384da8b7bbf39a0983981f52b06866fecf7a108
Abhimusmade010 <abhishekmusmade342@gmail.com>
2026-03-17 17:07:40 +0530

    make import correction for admin login check

--- src/components/AdminLogin.js
+++ src/components/AdminLogin.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
+// import React, { useState,  } from 'react';
 import { Link } from "react-router-dom";
 import Header from './Header';
 import API_BASE_URL from '../config/api';
